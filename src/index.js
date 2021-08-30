b350001c41c473d79d8301c5f9cf9e4723d26a17
aryanmajeed <aryanmajid97@gmail.com>
2021-08-30 14:13:21 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux"; /* code change */
 import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
+const store = createStore(counterReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); /* code change */
+// code change - added Provider to wrap around App
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider> /* code change */,
+  document.getElementById("root")
+);
 
-ReactDOM.render(<App />, document.getElementById("root"));
